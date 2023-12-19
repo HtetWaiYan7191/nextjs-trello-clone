698bfa7bebf52702c2cad2657e36e10f85c0c0c4
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-12-19 23:43:56 +0630

    Implement navbar in the project

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './_components/navbar'
 
 const MarketingPageLayout = ({children} : {children : React.ReactNode}) => {
   return (
     <div className='bg-slate-200 h-full'>
-        { /* navbar */}
+        <Navbar/>
         <main className=''>
             {children}
         </main>
