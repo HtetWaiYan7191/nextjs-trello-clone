89e46c5b9f16f68a1ca7e820caf1e403a6f937fd
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-12-21 14:19:51 +0630

    Implement org-control

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,11 +1,9 @@
-import { OrganizationSwitcher, auth } from '@clerk/nextjs'
 import React from 'react'
 
 const OrganizationIdPage = () => {
-  const {userId, orgId} = auth();
   return (
     <div>
-      <OrganizationSwitcher/>
+      Organization ID Page
     </div>
     
 
